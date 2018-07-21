2db3a6e62e2b1219b5bcb8f9d57f04d5875cee3f
Real cheese <dbtjdwocjswo@gmail.com>
2018-07-21 14:57:24 +0900

    Signed-off-by: Real cheese <dbtjdwocjswo@gmail.com>

--- simple_face_recognize.py
+++ simple_face_recognize.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 
-img_color = cv2.imread('C:\Users\samsung\PycharmProjects\project1\people.jpg', cv2.IMREAD_COLOR)
+img_color = cv2.imread('C:\Users\samsung\PycharmProjects\project1\project1\people.jpg', cv2.IMREAD_COLOR)
 gray_image = cv2.cvtColor(img_color,cv2.COLOR_BGR2GRAY)
-faceCascade = cv2.CascadeClassifier('C:\Users\samsung\PycharmProjects\project1\haar_face.xml')
+faceCascade = cv2.CascadeClassifier('C:\Users\samsung\PycharmProjects\project1\project1\haar_face.xml')
 faces = faceCascade.detectMultiScale(
     gray_image,
     scaleFactor=1.1,
@@ -12,7 +12,6 @@ faces = faceCascade.detectMultiScale(
 )
 
 
-#faces에 담긴 것을 xywh 에 나눠서 할당 하고 사각혁을 받아서 그린다. faces 에는 이미 분류를 통한 객체 사각혀잉 있다.
 print "FIND {0} faces!!".format(len(faces))
 for (x,y,w,h) in faces:
     cv2.rectangle(gray_image, (x,y), (x+w, y+h), (100,255,200), 2)
